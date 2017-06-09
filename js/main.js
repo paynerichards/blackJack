9aac37cc83101f368d599550a775bda0e04b66a9
Payne Richards <paynerichards@Paynes-MacBook-Pro.local>
2017-06-09 09:31:06 -0500

    fixed hand emptying issue

--- js/main.js
+++ js/main.js
@@ -5,59 +5,97 @@ var newDeck = function(){
 		url: "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6",
 		success: function(response){
 			deck = response.deck_id
-			console.log(deck)
+			// console.log(deck)
 		}
 	})
 }
 
 newDeck();
 
-var dealerHand = [];
-var playerHand = [];
+var gambler = {
+	hand: [],
+	score: 0,
+	handDisplay: $('.gamblerCards')
+}
+
+var dealer = {
+	hand: [],
+	score: 0,
+	handDisplay: $('.dealerCards')
+}
+
+
+
 var remain;
 
-var newCards = function(hand, amount){
+var newCards = function(player, amount){
 	$.ajax({
 		method:"GET",
 		url: "https://deckofcardsapi.com/api/deck/" + deck + "/draw/?count=" + amount,
 		success: function(response){
 			for(var i = 0; i < amount; i++){
-				hand.push(response.cards[i])
+				player.hand.push(response.cards[i])
 				remain = response.remaining
+				player.handDisplay.append('<img class=card src="' + response.cards[i].image + '">')
 			}
+			tallyScore(player)
+			// console.log(player.hand)
 		}
 	})
 }
 
-var checkWin = function(hand){
-	var score = 0;
-	for(var i = 0; i < hand.length; i++) {
-		var val = hand[i].value
+var tallyScore = function(player){
+	player.score = 0
+	for(var i = 0; i < player.hand.length; i++) {
+		var val = player.hand[i].value
 		if(val === "JACK" || val === "KING" || val === "QUEEN"){
-			score = score + 10
+			player.score = player.score + 10
+			// console.log("score" + player.score)
 		}else if(val === 'ACE'){
-			score = score + 11
-		}else score = score + Number(val)
-	}
-	console.log(score)
+			if(player.score <= 10){
+				player.score = player.score + 11
+				// console.log("score" + player.score)
+			}else if(player.score > 10){
+				player.score = player.score + 1
+				// console.log("score" + player.score)
+			}
+		}
+		else player.score += Number(val)
+			
+	}console.log("score" + player.score)
+	// console.log(player.score)
 }
 
-$('#dealBtn').click(function(){
-	if(playerHand.length === 0 & dealerHand.length === 0){
-		newCards(playerHand, 2);
-		newCards(dealerHand, 2);
-		console.log(playerHand);
-		console.log(dealerHand);
+var checkWin = function(){
+
+}
+
+var newRound = function(){
+	dealer.hand = [];
+	gambler.hand = [];
+	newCards(gambler, 2);
+	newCards(dealer, 2);
+	;
 	}
-	checkWin(playerHand);
-	checkWin(dealerHand);
+
+$('#dealBtn').click(function(){
+	
+	if(gambler.hand.length === 0 && dealer.hand.length === 0){
+		newRound();
+	}else if(gambler.score >= 21 || dealer.score >=21){
+		$('.card').remove()
+		newRound();
+
+	}
+	
 })
 
 $('#hitBtn').click(function(){
-	if(playerHand.length >= 2){
-		newCards(playerHand, 1)
-		console.log(playerHand)
+	if(gambler.hand.length >= 2 && gambler.score < 21 && dealer.score < 21){
+		// gambler.score = 0
+		newCards(gambler, 1)
 	}
+
 })
 
 
