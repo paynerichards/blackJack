aaff9e863e248771069f2ff20a2d2f7f4aef5b78
Payne Richards <paynerichards@Paynes-MacBook-Pro.local>
2017-06-14 12:22:57 -0500

    fixed ace scoring

--- js/main.js
+++ js/main.js
@@ -17,12 +17,20 @@ var gambler = {
 	hand: [],
 	score: 0,
 	handDisplay: $('.gamblerCards'),
+	valueDisplay: $('.countGam'),
 	wins: 0,
-	stand: false
+	stand: false,
+	over: false
 }
 
 var gamSplit = {
-	name: "split"
+	name: "split",
+	hand: [],
+	score: 0,
+	handDisplay: $('.splitCards'),
+	valueDisplay: $('.countSplit'),
+	toggle: false,
+	over: false
 }
 
 var dealer = {
@@ -30,6 +38,7 @@ var dealer = {
 	hand: [],
 	score: 0,
 	handDisplay: $('.dealerCards'),
+	valueDisplay: $('.countDeal'),
 	wins: 0,
 	secondCard: false
 }
@@ -37,6 +46,8 @@ var dealer = {
 var gameOver = false;
 
 var remain;
+
+var winner;
 
 var newCards = function(player, amount){
 	$.ajax({
@@ -53,11 +64,10 @@ var newCards = function(player, amount){
 			if(gambler.score >= 21 || dealer.score >= 21){
 				checkWin();
 			}
-			else if(dealer.score >= 17 && gambler.stand === true){
+			else if( (dealer.score >= 17) && gambler.stand === true){
 				checkWin();
 			}
-			hideCard();
-			
+			hideCard();			
 		}
 	})
 }
@@ -74,66 +84,87 @@ var tallyScore = function(player){
 	for(var i = 0; i < player.hand.length; i++) {
 		var val = player.hand[i].value
 		if(val === "JACK" || val === "KING" || val === "QUEEN"){
+			player.hand[i].value = "10"
 			player.score = player.score + 10
-		}else if(val === 'ACE'){
+		}
+		else if (val != 'ACE') player.score += Number(val)
+			
+	}
+	for(var i = 0; i < player.hand.length; i++){
+		var val = player.hand[i].value;
+		if(val === 'ACE'){
 			if(player.score <= 10){
 				player.score = player.score + 11
 			}else if(player.score > 10){
 				player.score = player.score + 1
 			}
 		}
-		else player.score += Number(val)
-			
-	}console.log(player.name + " score: " + player.score)
-	$('.valCounter').text('Value: ' + gambler.score)
+	}
+	console.log(player.name + " score: " + player.score)
+	gambler.valueDisplay.css('display', 'inherit')
+	valDisplay(player)
 }
 
 var checkWin = function(){
 	if(gambler.score > 21 || (dealer.score > gambler.score && dealer.score <= 21) && gameOver === false){
 		console.log('dealer Win');
 		dealer.wins++
+		winner = "Dealer"
 		$('#dealerScore').text('Dealer: ' + dealer.wins)
-		// alert('Dealer Wins :(')
 		gameOver = true;
 	}else if(dealer.score > 21 || (gambler.score >= dealer.score && gambler.score <= 21) && gameOver === false){
 		console.log('gambler Win');
 		gambler.wins++
 		$('#gamblerScore').text('Player: ' + gambler.wins)
-		// alert('Player Wins!')
+		winner = "Player";
 		gameOver = true;
 	}
+	dealer.valueDisplay.css('display', 'inherit')
+	winDisplay(winner)
 }
 
 var newRound = function(){
 	dealer.hand = [];
 	gambler.hand = [];
+	gamSplit.hand = [];
 	dealer.score = 0;
-	gambler.score = 0
+	gambler.score = 0;
+	gamSplit.score = 0;
 	newCards(gambler, 2);
 	newCards(dealer, 2);
 	gambler.stand = false;
+	gamSplit.toggle = false;
 	gameOver = false;
-	;
+	gamSplit.valueDisplay.hide();
+	dealer.valueDisplay.hide();
+	$('.winScreen').remove();
 }
 
 var hideCard = function(){
-	if(dealer.hand.length === 2 && gambler.hand.length === 2 && gameOver === false){
+	if(dealer.hand.length === 2 && gambler.hand.length === 2 && gameOver === false && gamSplit.toggle === false ){
 		$($('.card')[1]).hide()
 		dealer.handDisplay.append('<img id="hiddenCard" src="media/cardBack.jpg">')
 	}else if(dealer.hand.length === 2 && gameOver === false){
 		$($('.card')[1]).hide()		
 	}
-
 	else{
 		$('#hiddenCard').remove()
 		$($('.card')[1]).show()
+		valDisplay(dealer)
 
 	}
 }
 
+var valDisplay = function(player){
+	player.valueDisplay.text('Value: ' + player.score)
+}
+
+var winDisplay = function(winner){
+	$('body').append('<div class="winScreen"><div class="winText">' + winner +' Wins!</div></div>')
+}
+
 
 $('#dealBtn').click(function(){
-	
 	if(gambler.hand.length === 0 && dealer.hand.length === 0){
 		console.log('deal')	
 		newRound();
@@ -142,41 +173,67 @@ $('#dealBtn').click(function(){
 		console.log('deal')
 		newRound();
 		gameOver = false;
-
-	}
-
-	
+	}	
 })
 
-$('#hitBtn').click(function(){
-	
-	if(gambler.hand.length >= 2 && gambler.score < 21 && dealer.score < 21 && gameOver === false){
+
+$('#hitBtn').click(function(){	
+	if(gambler.hand.length >= 2 && gambler.score < 21 && dealer.score < 21 && gameOver === false && gambler.stand === false){
 		console.log('hit');
 		newCards(gambler, 1);
 	}
-
 })
 
 $('#standBtn').click(function(){
 	if(dealer.score < 17 && gambler.score <= 21 && gameOver === false){
 		console.log('stand');
 		newCards(dealer, 1);
-		
+		// do {
+		// 	newCards(dealer, 1);
+		// 	debugger;
+		// } while(dealer.score < 17)
+
+		// while(dealer.score < 17){
+		// 	newCards(dealer, 1)
+		// 	debugger;
+		// }		
 	}
-	else if( gameOver === false){
+	else if(gameOver === false){
 		console.log('stand');
 		checkWin()
 		hideCard()
 	}
 	gambler.stand = true;
+	dealer.valueDisplay.css('display', 'inherit')
+})
+
+$('#splitBtn').click(function(){
+	if(gambler.hand[0].value === gambler.hand[1].value){
+		gamSplit.toggle = true
+		console.log('split')
+		gamSplit.hand.push(gambler.hand[1]);
+		gambler.hand.pop();
+		$('.splitCards').append($('.card')[3])
+		newCards(gamSplit, 1)
+		newCards(gambler, 1)
+		gamSplit.valueDisplay.css("display", "inherit");		
+	}
 })
 
 
 
 
+// gambler.score < dealer.score &&
 
 
-// gambler.score > dealer.score &&
+
+
+
+
+
+
+
+
 
 
 
